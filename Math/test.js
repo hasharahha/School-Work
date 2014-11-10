6c133ed83e9209907bb6cbff6afd06beb5d1ad56
Jacob Bondley <hasharahha@gmail.com>
2014-11-10 09:59:17 -0500

    Started test.js

---
+++ Math/test.js
@@ -0,0 +1,5 @@
+var Questions =["what is a Linear Equation?","
+var Answers =[];
+Answers[0].A1="An equation Between two Variables";
+Answer[0].A2="An equation that has 0 variables";
+Answer[0].A3="
