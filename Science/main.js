9f490f4d7c84ab04cf45faf0c724359aaf42f5c1
Jacob Bondley <hasharahha@gmail.com>
2014-11-10 18:00:16 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,10 +1,13 @@
 alert("Loading Water index")
 
 function search(){
-  alert("Checking Server....")
+  
 var key = document.getElementById("search").value;
 search = key.toLowerCase();
 if(search==="evaporation"){
 document.getElementById("definition").innerHTML="The Process Changing of liquid into a gas";
+}
+else{
+  alert("error Code 404 not found")
 }
 }
