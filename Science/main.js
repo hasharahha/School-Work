4aeb0330b72d1179f849deee561dc8b5797de9fb
Jacob Bondley <hasharahha@gmail.com>
2014-11-10 18:13:23 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,11 +1,18 @@
 alert("Loading Water index")
-
+var Define ={};
+var Example ={};
+Define.evaporation="The Process of Changing a liquid to gas.";
+Define.precipitation="Rain, Sleet or Snow any form of liquid or solid that falls from the sky."
 function search(){
   
 var key = document.getElementById("search").value;
 search = key.toLowerCase();
 if(search==="evaporation"){
-document.getElementById("definition").innerHTML="The Process Changing of liquid into a gas";
+document.getElementById("definition").innerHTML="Definition:"+" "+Define.evaporation;
+}
+else if(search==="precipitation"){
+  
+}
 }
 else{
   alert("error Code 404 not found")
