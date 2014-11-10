f6e9e97e48b54abcdb98927f17a255f1c5261afa
Jacob Bondley <hasharahha@gmail.com>
2014-11-10 18:22:01 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -2,7 +2,12 @@ alert("Loading Water index")
 var Define ={};
 var Example ={};
 Define.evaporation="The Process of Changing a liquid to gas.";
-Define.precipitation="Rain, Sleet or Snow any form of liquid or solid that falls from the sky."
+Define.precipitation="Rain, Sleet or Snow any form of liquid or solid that falls from the sky.";
+Define.tributaries="A River or stream crossing into a larger river or lake";
+Define.lake="A large body of water surrounded by land";
+Define.river="A large natural stream of water flowing into a ocean or lake.";
+Define.groundwater="Water found underground in soil in other words and aquifier.";
+Define.percolation="The process of water slowly passing through a filter";
 function search(){
   
 var key = document.getElementById("search").value;
