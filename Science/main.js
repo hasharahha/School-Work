728885133298d0433117cae09858a4207e9d268c
Jacob Bondley <hasharahha@gmail.com>
2014-11-10 15:24:22 -0500

    Create main.js

---
+++ Science/main.js
@@ -0,0 +1,6 @@
+function search(){
+var key = document.getElementById("search").value;
+key = key.toLowerCase();
+if(key=="evaporation"){
+document.getElementById("definition").innerHTML="The Changing of liquid into a gas";
+}
