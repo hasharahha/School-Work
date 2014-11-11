f3e2f2e610fff6ff2f6dfdfc981d01035d47b18d
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 15:49:06 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,4 +1,4 @@
-alert("Loading Water index V5.0")
+alert("Loading Water index V5.1")
 var Define =["undefined","The Process of Changing a liquid to gas.","Rain, Sleet or Snow any form of liquid or solid that falls from the sky.","A River or stream crossing into a larger river or lake","A large body of water surrounded by land","A large natural stream of water flowing into a ocean or lake.","Water found underground in soil in other words and aquifier.","The process of water slowly passing through a filter","The source or headwaters of a river or stream is the furthest place in that river or stream from its estuary or confluence with another river, as  ","a very large amount of water all in one place","The conversion of water to a liquid, on a cold surface","An area of low lying ground near water or stream"," a crescent-shaped often ephemeral lake formed in the abandoned channel of a meander by the silting up of its ends after the stream has cut through the land within the meander at a narrow point.","Saturated Land, swamps ect.","a long rane hill top, mountain range , or water shed","a winding curve of a river","Delta is   a landform that forms at the mouth of a river, where the river flows into an ocean, sea, estuary, lake, or reservoir. Deltas form from deposition of sediment carried by a river as the flow leaves its mouth."," Transpiration is the process of water movement through a plant and its evaporation from aerial parts, such as from leaves but also from stems and flowers. Leaf surfaces are dotted with pores called stomata, and in most plants they are more numerous on the undersides of the foliage. "];
 function search(keyword){
   var search = keyword.toLowerCase();
@@ -108,5 +108,5 @@ else{
   document.getElementById("searchimg").style.backgroundPosition='0px -1350px';
 }
 }
-
+//end of search engine checker
 
