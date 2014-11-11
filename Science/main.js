5bd9525f7a6ec3715fea6bebdbf94d65d3f48036
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 10:32:22 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,5 +1,5 @@
 alert("Loading Water index")
-var Define =["undefined","The Process of Changing a liquid to gas.","Rain, Sleet or Snow any form of liquid or solid that falls from the sky.","A River or stream crossing into a larger river or lake","A large body of water surrounded by land","A large natural stream of water flowing into a ocean or lake.","Water found underground in soil in other words and aquifier.","The process of water slowly passing through a filter"];
+var Define =["undefined","The Process of Changing a liquid to gas.","Rain, Sleet or Snow any form of liquid or solid that falls from the sky.","A River or stream crossing into a larger river or lake","A large body of water surrounded by land","A large natural stream of water flowing into a ocean or lake.","Water found underground in soil in other words and aquifier.","The process of water slowly passing through a filter","a very large amount of water all in one place","The conversion of water to a liquid, on a cold surface"];
 function search(keyword){
   var search = keyword.toLowerCase();
   if(search==="evaporation"){
