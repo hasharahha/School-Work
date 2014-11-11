0724f4f5309a15d50aaaca5a86a2aab108fcb9e8
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 09:22:16 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,13 +1,14 @@
 alert("Loading Water index")
 var Define =[];
 var Example ={};
-Define[1]="The Process of Changing a liquid to gas.";
-Define[2]="Rain, Sleet or Snow any form of liquid or solid that falls from the sky.";
-Define[3]="A River or stream crossing into a larger river or lake";
-Define[4]="A large body of water surrounded by land";
-Define[5]="A large natural stream of water flowing into a ocean or lake.";
-Define[6]="Water found underground in soil in other words and aquifier.";
-Define[7]="The process of water slowly passing through a filter";
+Define[0] ="undefined"
+Define[1] ="The Process of Changing a liquid to gas.";
+Define[2] ="Rain, Sleet or Snow any form of liquid or solid that falls from the sky.";
+Define[3] ="A River or stream crossing into a larger river or lake";
+Define[4] ="A large body of water surrounded by land";
+Define[5] ="A large natural stream of water flowing into a ocean or lake.";
+Define[6] ="Water found underground in soil in other words and aquifier.";
+Define[7] ="The process of water slowly passing through a filter";
 function search(){
   
 var key = document.getElementById("search").value;
