353cf4b0f5dde3c62ac214ed3fc775effd39b4bd
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 09:33:12 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,26 +1,6 @@
 alert("Loading Water index")
-var Define =[];
-var Example ={};
-Define[0] ="undefined"
-Define[1] ="The Process of Changing a liquid to gas.";
-Define[2] ="Rain, Sleet or Snow any form of liquid or solid that falls from the sky.";
-Define[3] ="A River or stream crossing into a larger river or lake";
-Define[4] ="A large body of water surrounded by land";
-Define[5] ="A large natural stream of water flowing into a ocean or lake.";
-Define[6] ="Water found underground in soil in other words and aquifier.";
-Define[7] ="The process of water slowly passing through a filter";
-function search(){
+var Define =["undefined","The Process of Changing a liquid to gas.","Rain, Sleet or Snow any form of liquid or solid that falls from the sky.","A River or stream crossing into a larger river or lake","A large body of water surrounded by land","A large natural stream of water flowing into a ocean or lake.","Water found underground in soil in other words and aquifier.","The process of water slowly passing through a filter"];
+function search(keyword){
   
-var key = document.getElementById("search").value;
-search = key.toLowerCase();
-if(search==="evaporation"){
-document.getElementById("definition").innerHTML="Definition: "+Define[1]
-}
-else if(search==="precipitation"){
   
-}
-}
-else{
-  alert("error Code 404 not found")
-}
 }
