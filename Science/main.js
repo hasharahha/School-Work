9fa80ef3750a78d87b60258ca5b24ec63b1a9a2f
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 09:37:33 -0500

    Update main.js

--- Science/main.js
+++ Science/main.js
@@ -1,6 +1,7 @@
 alert("Loading Water index")
 var Define =["undefined","The Process of Changing a liquid to gas.","Rain, Sleet or Snow any form of liquid or solid that falls from the sky.","A River or stream crossing into a larger river or lake","A large body of water surrounded by land","A large natural stream of water flowing into a ocean or lake.","Water found underground in soil in other words and aquifier.","The process of water slowly passing through a filter"];
 function search(keyword){
+  var search = keyord.toLowerCase();
   
   
 }
