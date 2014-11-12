00374a180178ada58097b1a2e65de005949551eb
Jacob Bondley <hasharahha@gmail.com>
2014-11-12 07:01:56 -0500

    Update test.js

--- Math/test.js
+++ Math/test.js
@@ -1,20 +1,15 @@
-
-var answer1 = document.getElementById("Q1A").value;
-var answer2 = document.getElementById("Q2B").value;
-var answer3 = document.getElementById("Q3D").value;
-var score = 0;
-function Score(){
+//Checker for test
+var score =0;
+function Check1(){
   if(document.getElementById("Q1A").checked){
-    score += 10;
-  }
-  switch(score){
-    case document.getElementById("Q2B").checked:
-      score +=10;
-      break;
-    case document.getElementById("Q3D").checked;
-      sore +=10;
-      break;
-    
+    score +=10;
   }
 }
-}
+ funtion Check2(){
+   if(document.getElementById("Q2B").checked){
+     score +10;
+   }
+ }
+ function Check3(){
+   if(document.getElememtById("Q"))
+ }
