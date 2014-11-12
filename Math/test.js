00b48a1e62df6359c407de4172cd16c0ccbfdee3
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 21:26:23 -0500

    Update test.js

--- Math/test.js
+++ Math/test.js
@@ -4,7 +4,10 @@ var answer2 = document.getElementById("Q2B").value;
 var answer3 = document.getElementById("Q3D").value;
 var score;
 function Score(){
-  if(document.getElementById("Q1A").checked==="true"){
+  if(document.getElementById("Q1A").checked){
+    score += 10;
+  }
+  if(document.getElementById("Q2B").checked){
     score += 10;
   }
 }
