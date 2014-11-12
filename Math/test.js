d0c7ea2591fa631196ef2d1ae241841ef82ceb03
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 21:22:02 -0500

    Update test.js

--- Math/test.js
+++ Math/test.js
@@ -1,5 +1,10 @@
-var Questions =["what is a Linear Equation?","
-var Answers =[];
-Answers[0].A1="An equation Between two Variables";
-Answer[0].A2="An equation that has 0 variables";
-Answer[0].A3="
+
+var answer1 = document.getElementById("Q1A").value;
+var answer2 = document.getElementById("Q2B").value;
+var answer3 = document.getElementById("Q3D").value;
+var score;
+function Score(){
+  if(document.getElementById("Q1A").checked==="true"){
+    score += 10;
+  }
+}
