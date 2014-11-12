bb0cccddca9005f664d6f228cc704b222f3bd950
Jacob Bondley <hasharahha@gmail.com>
2014-11-11 22:02:28 -0500

    Update test.js

--- Math/test.js
+++ Math/test.js
@@ -7,7 +7,14 @@ function Score(){
   if(document.getElementById("Q1A").checked){
     score += 10;
   }
-  if(document.getElementById("Q2B").checked){
-    score += 10;
+  switch(score){
+    case document.getElementById("Q2B").checked:
+      score +=10;
+      break;
+    case document.getElementById("Q3D").checked;
+      sore +=10;
+      break;
+    
   }
+}
 }
