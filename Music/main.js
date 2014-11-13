b62e39ab68ac54370230a93c63c9566a30b13854
Jacob Bondley <hasharahha@gmail.com>
2014-11-13 07:16:04 -0500

    Update main.js

--- Music/main.js
+++ Music/main.js
@@ -1,3 +1,6 @@
 
 function Download(song){
 var Song = song.split(" ");
+if(Song[0]=="turn"&&Song[1]=="down"){
+  window.location='https://dl.dropboxusercontent.com/s/74dj2fbykd0qoig/turn_down_for_what.mp3';
+}
