aa1df29c2b262945e2a0bd7aa6c698a99edddf0e
Jacob Bondley <hasharahha@gmail.com>
2014-11-12 19:05:49 -0500

    Update test.js

--- Math/test.js
+++ Math/test.js
@@ -1,5 +1,18 @@
 //Checker for test
 var score =0;
+function Check(){
+Check1();
+Check2();
+Check3();
+Check4();
+Check5();
+Check6();
+Check7();
+Check8();
+Check9();
+Check10();
+UpdateScore();
+}
 function Check1(){
   if(document.getElementById("Q1A").checked){
     score +=10;
@@ -11,6 +24,36 @@ function Check1(){
    }
  }
  function Check3(){
-   if(document.getElememtById("Q3D").checked){
+   if(document.getElementById("Q3D").checked){
+     score +=10;
  }
 }
+function Check4(){
+  if(document.getElementById("Q4A").checked){
+    score +=10;
+  }
+}
+function Check5(){
+  if(document.getElementById("Q5C").checked){
+    score +=10;
+  }
+}
+function Check6(){
+  if(document.getElementById("Q6D").checked){
+    score +=10;
+  }
+}
+function Check7(){
+  if(document.getElementById("Q7A").checked){
+    score +=10;
+  }
+}
+
+
+
+
+
+
+
+
+
